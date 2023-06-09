e7689927e487c494ccdb0bbe22649893258b9d07
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-06-09 11:06:20 +0600

    users api add

--- index.js
+++ index.js
@@ -32,7 +32,30 @@ async function run() {
       .db("summerCamp")
       .collection("popularInstructors");
 
+    //  JWT TOKEN secure
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send(token);
+    });
+
     // users related apis -----------------------
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // save user email and role in DB
     app.put("/users/:email", async (req, res) => {
@@ -44,6 +67,13 @@ async function run() {
         $set: user,
       };
       const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(filter);
       res.send(result);
     });
 
