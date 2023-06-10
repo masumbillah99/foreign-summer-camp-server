999e018cb770b094465e7bb4d01399ae6a68ebb9
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-06-10 13:11:57 +0600

    add jwt token

--- index.js
+++ index.js
@@ -10,6 +10,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// verify jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized Access" });
+  }
+  // bearer 'token'
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+/**
+ * const decodeEmail = req.decoded.email;
+ * if(email !== decodedEmail) {
+ *   return res
+        .status(403)
+        .send({ error: true, message: "Forbidden Access" });
+ * }
+*/
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.og57wk2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,15 +66,30 @@ async function run() {
     //  JWT TOKEN secure
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      res.send(token);
+      res.send({ token });
     });
 
     // users related apis -----------------------
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // check users ? admin
+    // email name
+    // check admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      console.log(result);
       res.send(result);
     });
 
